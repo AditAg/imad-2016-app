e08b15d7529f7bfa2383b3d3c58c6865acd213c4
AditAg <aditagarwal1997@gmail.com>
2016-09-24 17:02:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,5 @@
 console.log('Loaded!');
+//Change the text of the main-text of the page
+
+var element=document.getElementById('main-text');
+element.innerHTML="Hello this is the first page of the website";
