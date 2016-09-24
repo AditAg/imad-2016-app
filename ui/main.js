374d7fb5ee687c9ea98dcc3d1a9a2fdf0e36f865
AditAg <aditagarwal1997@gmail.com>
2016-09-24 17:19:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,16 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML="Hello this is the first page of the website";
+
+//move the  image
+var image=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=arginLeft+5;
+    image.style.marginLeft=marginLeft +'px';
+}
+
+image.onclick=function(){
+    var interval=setInterval(moveRight,50);
+    
+};
