1ac1d6bc04af300161da40f58e2405b4fb5421d3
AditAg <aditagarwal1997@gmail.com>
2016-09-27 23:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ button.onclick=function(){
     var request = new XMLHttpRequest();
     //Capture the response and store it in a variable
     request.onreadystatechange=function(){
-        if(request.readystate === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE){
             //Take some action'
             if(request.status===200){
                 var counter=request.responseText;
@@ -66,7 +66,7 @@ submit.onclick=function(){
     var request = new XMLHttpRequest();
     //Capture the response and store it in a variable
     request.onreadystatechange=function(){
-        if(request.readystate === XMLHttpRequest.DONE){
+        if(request.readyState === XMLHttpRequest.DONE){
             //Take some action'
             if(request.status===200){
                //Capture a list of names and render it as a list
