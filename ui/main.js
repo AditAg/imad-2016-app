b05ecb44ef97aadb43271c89abaa9149e381230d
AditAg <aditagarwal1997@gmail.com>
2016-09-27 20:27:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,31 @@ image.onclick=function(){
     var interval=setInterval(x,50);
     
 };
+
+//counter code
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    
+    //Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Take some action'
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();  
+            }
+        }
+        //Not done yet
+        
+    };
+    //Render the variable in the correct span
+    
+    //Make the request
+    request.open('GET','http://aditag.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
